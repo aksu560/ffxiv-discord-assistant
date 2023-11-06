20222939133471ab3cc6f4d9e5df0a4864e9094f
Aksu <aksu@venho.net>
2023-11-06 07:00:23 +0200

    Restructured item requests to be more cacheable.

--- src/xiv.rs
+++ src/xiv.rs
@@ -1,7 +1,7 @@
 use std::env;
 use cached::proc_macro::{cached};
 use serde::Deserialize;
-use reqwest::get;
+use reqwest::{get};
 
 #[allow(dead_code)]
 struct XivConfig {
@@ -24,18 +24,24 @@ fn request_url_builder(endpoint: &str, item: String) -> String {
     endpoint.to_string() + &*item + "?private_key=" + &*get_api_key()
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Default, Clone)]
 struct ItemData {
     #[serde(rename="Name_en")]
     name_en: String
 }
 
+impl ItemData {
+    pub fn get_name(&self) -> String {
+        self.name_en.clone()
+    }
+}
+
 #[cached(time=86400, sync_writes=true)]
-async fn id2name(id: String) -> String {
+async fn get_item_data(id: String) -> ItemData {
     let response = get(request_url_builder("https://xivapi.com/item/", id)).await;
     match response {
-        Ok(r) => r.json::<ItemData>().await.unwrap().name_en,
-        Err(..) => "".to_string()
+        Ok(r) => r.json::<ItemData>().await.unwrap(),
+        Err(..) => ItemData::default()
     }
 }
 
@@ -46,14 +52,14 @@ mod tests {
     #[tokio::test]
     async fn test_item2id() {
         assert_eq!(
-            id2name("5503".to_string()).await,
+            get_item_data("5503".to_string()).await.get_name(),
             "Animal Glue"
         );
     }
     #[tokio::test]
     async fn test_item2id_again() {
         assert_eq!(
-            id2name("5503".to_string()).await,
+            get_item_data("5503".to_string()).await.get_name(),
             "Animal Glue"
         );
     }
